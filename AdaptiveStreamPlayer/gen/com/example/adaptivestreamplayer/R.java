8a529abaca4bc29449ca6fe36eb69778df35d5a1
tuanna55 <dinobot92t@gmail.com>
2013-09-16 13:36:38 +0700

    new UI

--- AdaptiveStreamPlayer/gen/com/example/adaptivestreamplayer/R.java
+++ AdaptiveStreamPlayer/gen/com/example/adaptivestreamplayer/R.java
@@ -24,18 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int back=0x7f080005;
-        public static final int default_player=0x7f080001;
-        public static final int default_tests=0x7f080002;
-        public static final int server_tests=0x7f080003;
-        public static final int settings=0x7f080004;
-        public static final int videoView=0x7f080000;
+        public static final int back=0x7f080008;
+        public static final int custom_url=0x7f080003;
+        public static final int default_player=0x7f080004;
+        public static final int default_tests=0x7f080005;
+        public static final int pager=0x7f080000;
+        public static final int pager_title_strip=0x7f080001;
+        public static final int server_tests=0x7f080006;
+        public static final int settings=0x7f080007;
+        public static final int videoView=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_play_video=0x7f030000;
-        public static final int default_play=0x7f030001;
-        public static final int list_video=0x7f030002;
-        public static final int settings=0x7f030003;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_play_video=0x7f030001;
+        public static final int custom_uri=0x7f030002;
+        public static final int default_play=0x7f030003;
+        public static final int list_video=0x7f030004;
+        public static final int settings=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -44,16 +49,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f05000b;
         public static final int app_name=0x7f050000;
-        public static final int back=0x7f050003;
-        public static final int custom_player=0x7f050008;
-        public static final int default_player=0x7f050007;
-        public static final int default_tests=0x7f050005;
+        public static final int back=0x7f050004;
+        public static final int custom_player=0x7f050009;
+        public static final int default_player=0x7f050008;
+        public static final int default_tests=0x7f050006;
         public static final int first_video=0x7f050001;
-        public static final int hello_world=0x7f050009;
-        public static final int server_tests=0x7f050006;
-        public static final int settings=0x7f050004;
-        public static final int title_activity_main=0x7f050002;
-        public static final int title_activity_main_activity1=0x7f05000a;
+        public static final int hello_world=0x7f05000a;
+        public static final int play=0x7f050002;
+        public static final int server_tests=0x7f050007;
+        public static final int settings=0x7f050005;
+        public static final int title_activity_main=0x7f050003;
         public static final int title_section1=0x7f05000c;
         public static final int title_section2=0x7f05000d;
         public static final int title_section3=0x7f05000e;
--- AdaptiveStreamPlayer/src/com/example/adaptivestreamplayer/MainActivity.java
+++ AdaptiveStreamPlayer/src/com/example/adaptivestreamplayer/MainActivity.java
@@ -1,47 +1,50 @@
 package com.example.adaptivestreamplayer;
 
-import android.app.Activity;
+import java.util.Locale;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	boolean defaultPlayer;
 	public static String EXTRA_URL = "EXTRA_URL";
 	private String url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
+	private SectionsPagerAdapter mSectionsPagerAdapter;
+	private ViewPager mViewPager;
+	private String TAG = "MainActivity";
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.list_video);
+		setContentView(R.layout.activity_main);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager());
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.default_tests:
-				setContentView(R.layout.list_video);
-				return true;
-			case R.id.server_tests:
-				setContentView(R.layout.list_video);
-				return true;
-			case R.id.settings:
-				showSetting();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}    
 	}
 	
 	public void showSetting(){
@@ -67,4 +70,83 @@ public class MainActivity extends Activity {
 		startActivity(intent);		
 	}
 	
+	public void customUrl(View view){
+		EditText editText = (EditText) findViewById(R.id.custom_url);
+		String url = editText.getText().toString();
+		if(defaultPlayer){
+			Intent intent = new Intent(this, DefaultPlayerActivity.class);
+			Log.e(TAG,"custom url: " + url);
+			intent.putExtra(EXTRA_URL, url);
+			startActivity(intent);
+			return;
+		}
+		Intent intent = new Intent(this, CustomPlayActivity.class);
+		intent.putExtra(EXTRA_URL, url);
+		startActivity(intent);		
+	}
+
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fragment = new DummySectionFragment();
+			Bundle args = new Bundle();
+			int layout;
+			layout = R.layout.list_video;
+			if(position == 1)
+				layout = R.layout.custom_uri;
+			if(position == 2)
+				layout = R.layout.settings;
+			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, layout);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			// Show 3 total pages.
+			return 3;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return getString(R.string.title_section1).toUpperCase(l);
+			case 1:
+				return getString(R.string.title_section2).toUpperCase(l);
+			case 2:
+				return getString(R.string.title_section3).toUpperCase(l);
+			}
+			return null;
+		}
+	}
+
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class DummySectionFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		public static final String ARG_SECTION_NUMBER = "section_number";
+
+		public DummySectionFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			int layout = getArguments().getInt(ARG_SECTION_NUMBER);
+			View rootView = inflater.inflate(layout, container, false);
+			return rootView;
+		}
+	}
 }
